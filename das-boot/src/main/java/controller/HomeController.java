77fcb3961b9b7f72f7eef566e32b518b6127fc9a
Vinod Choudhary <vchou5@sapient.com>
2016-04-06 14:42:34 +0530

    My First Spring Boot Application

---
+++ das-boot/src/main/java/controller/HomeController.java
@@ -0,0 +1,14 @@
+package controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Spiky, Enter into New World";
+	}
+
+}
